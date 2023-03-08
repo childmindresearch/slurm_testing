41f660afd4ca9abf940a5b19391a003fd13c4486
Amy Gutierrez <58920810+amygutierrez@users.noreply.github.com>
2023-03-08 11:50:09 -0500

    Add files via upload

---
+++ regression_run_scripts/regtest_correlations.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+#SBATCH -N 1
+#SBATCH -p RM-shared
+#SBATCH -t 24:00:00
+#SBATCH --ntasks-per-node=20
+
+preconfig="default"
+
+cd /ocean/projects/med220004p/agutierr/cpac_v1.8.4_VS_v1.8.5_corr_final
+
+cat << TMP > regtest_correlations_${preconfig}.sh
+
+#!/usr/bin/bash
+echo "Starting Correlations"
+python3 /ocean/projects/med220004p/agutierr/CPAC_regtest_pack/cpac_correlations.py \
+/ocean/projects/med220004p/agutierr/corr_yml_regtests/v1.8.5/corrtest_${preconfig}_HNU_1.yml
+
+python3 /ocean/projects/med220004p/agutierr/CPAC_regtest_pack/cpac_correlations.py \
+/ocean/projects/med220004p/agutierr/corr_yml_regtests/v1.8.5/corrtest_${preconfig}_KKI.yml
+
+python3 /ocean/projects/med220004p/agutierr/CPAC_regtest_pack/cpac_correlations.py \
+/ocean/projects/med220004p/agutierr/corr_yml_regtests/v1.8.5/corrtest_${preconfig}_Site-CBIC.yml
+
+python3 /ocean/projects/med220004p/agutierr/CPAC_regtest_pack/cpac_correlations.py \
+/ocean/projects/med220004p/agutierr/corr_yml_regtests/v1.8.5/corrtest_${preconfig}_Site-SI.yml
+
+echo "Finished correlation ${preconfig}"
+TMP
+        chmod +x regtest_correlations_${preconfig}.sh
+
+singularity exec -B /ocean/projects/med220004p/agutierr:/ocean/projects/med220004p/agutierr \
+--cleanenv /ocean/projects/med220004p/agutierr/c-pac_nightly-v1.8.5.sif regtest_correlations_${preconfig}.sh
