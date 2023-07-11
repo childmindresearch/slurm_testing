105979294467c450550579b02e74baa8420f4160
Theodore (Machine User) <CMI_CPAC_Support@childmind.org>
2023-07-11 16:10:34 -0400

    added script to build singulairty image

---
+++ regression_run_scripts/build_image.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --working_dir) working_dir="$2"; shift ;;
+        --image) image="$2"; shift ;;
+    esac
+    shift
+done
+
+cat << TMP > build_${image}.sh
+#!/usr/bin/bash
+#SBATCH -N 1
+#SBATCH -p RM-shared
+#SBATCH -t 1:00:00
+#SBATCH --ntasks-per-node=20
+
+SINGULARITY_CACHEDIR=${working_dir}/.singularity/cache \
+SINGULARITY_LOCALCACHEDIR=${working_dir}/.singularity/tmp \
+singularity build ${image}.sif docker://${image}
+
+TMP
+
+chmod +x build_${image}.sh
+sbatch build_${image}.sh
