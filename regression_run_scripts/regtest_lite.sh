7b1056ee65c156d395262bcd1d0018968fc5ca01
Jon Clucas <jon.clucas@childmind.org>
2023-07-25 13:53:58 -0400

    :construction_worker: Separate run launch from individual runs

--- regression_run_scripts/regtest_lite.sh
+++ regression_run_scripts/regtest_lite.sh
@@ -15,39 +15,40 @@ done
 IMAGE_NAME=${image#*:}
 GIT_REPO=${HOME_DIR}/slurm_testing
 GIT_REPO_TEST=${HOME_DIR}/slurm_testing_callback
-DATA=${HOME_DIR}/DATA/reg_5mm_pack
+DATA_DIR=${HOME_DIR}/DATA/reg_5mm_pack
 OUT=${HOME_DIR}/${IMAGE_NAME}
 IMAGE=${IMAGE_NAME}.sif
-PIPELINE_CONFIGS=${DATA}/configs
+PIPELINE_CONFIGS=${DATA_DIR}/configs
 PRECONFIGS="default"
 DATA_SOURCE="Site-CBIC Site-SI HNU_1"
 
 cd $GIT_REPO_TEST
 
-for pipeline in ${PRECONFIGS}; do
-    for data in ${DATA_SOURCE}; do
-        if [ ${data} == 'HNU_1' ]; then
+for PIPELINE in ${PRECONFIGS}; do
+    for DATA in ${DATA_SOURCE}; do
+        if [ ${DATA} == 'HNU_1' ]; then
             subject="sub-0025428"
-            datapath=${DATA}/data/HNU_1
-        elif [ ${data} == 'Site-CBIC' ]; then
+            datapath=${DATA_DIR}/data/HNU_1
+        elif [ ${DATA} == 'Site-CBIC' ]; then
             subject="sub-NDARAB348EWR"
-            datapath=${DATA}/data/Site-CBIC
-        elif [ ${data} == 'Site-SI' ]; then
+            datapath=${DATA_DIR}/data/Site-CBIC
+        elif [ ${DATA} == 'Site-SI' ]; then
             subject="sub-NDARAD481FXF"
-            datapath=${DATA}/data/Site-SI
+            datapath=${DATA_DIR}/data/Site-SI
         fi
 
-        OUTPUT=${OUT}/${pipeline}/${data}
+        OUTPUT=${OUT}/${PIPELINE}/${DATA}
         [ ! -d  ${OUTPUT} ] && mkdir -p ${OUTPUT}
 
-        cat << TMP > reglite_${IMAGE_NAME}_${pipeline}_${data}.sh
+        cat << TMP > reglite_${IMAGE_NAME}_${PIPELINE}_${DATA}.sh
 #!/usr/bin/bash
 #SBATCH -N 1
 #SBATCH -p RM-shared
 # #SBATCH -t 10:00:00
 # #SBATCH --ntasks-per-node=11
-#SBATCH -o slurm-${pipeline}-${data}.out
-#SBATCH --wrap "OWNER=$OWNER REPO=$REPO SHA=$SHA HOME_DIR=$HOME_DIR ./$GIT_REPO/.github/scripts/status.SLRUM"
+#SBATCH -o slurm-${PIPELINE}-${DATA}.out
+#SBATCH --error slurm-${PIPELINE}-${DATA}.err
+#SBATCH --wrap "OWNER=$OWNER REPO=$REPO SHA=$SHA HOME_DIR=$HOME_DIR PRECONFIG=$PIPELINE DATA_SOURCE=$DATA ./$GIT_REPO/.github/scripts/status.SLURM"
 
 # SINGULARITY_CACHEDIR=${HOME_DIR}/.singularity/cache \
 # SINGULARITY_LOCALCACHEDIR=${HOME_DIR}/.singularity/tmp \
@@ -61,14 +62,14 @@ for pipeline in ${PRECONFIGS}; do
 #     -B ${PIPELINE_CONFIGS}:/pipeline_configs \
 #     ${IMAGE} /data /outputs participant \
 #     --save_working_dir --skip_bids_validator \
-#     --pipeline_file /pipeline_configs/${pipeline}_lite.yml \
+#     --pipeline_file /pipeline_configs/${PIPELINE}_lite.yml \
 #     --participant_label ${subject} \
 #     --n_cpus 10 --mem_gb 40
-# TMP
-#         chmod +x reglite_${IMAGE_NAME}_${pipeline}_${data}.sh
-#         sbatch reglite_${IMAGE_NAME}_${pipeline}_${data}.sh
-#     done
-# done
+TMP
+#         chmod +x reglite_${IMAGE_NAME}_${PIPELINE}_${DATA}.sh
+#         sbatch reglite_${IMAGE_NAME}_${PIPELINE}_${DATA}.sh
+    done
+done
 
 rm reglite_${IMAGE_NAME}_*.sh
 # rm ${IMAGE}
