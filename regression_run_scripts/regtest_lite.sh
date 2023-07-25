51442208acb9cf64ec6c65da361f2a46d35dd0a9
Jon Clucas <jon.clucas@childmind.org>
2023-07-25 12:00:28 -0400

    :construction_worker: Update check based on SLURM status

--- regression_run_scripts/regtest_lite.sh
+++ regression_run_scripts/regtest_lite.sh
@@ -2,7 +2,7 @@
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --home_dir) home_dir="$2"; shift ;;
+        --home_dir) HOME_DIR="$2"; shift ;;
         --image) image="$2"; shift ;;
         --path-extra) export PATH="$PATH:$2"; shift ;;
         --sha) SHA="$2"; shift ;;
@@ -13,55 +13,49 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 IMAGE_NAME=${image#*:}
-GIT_REPO=${home_dir}/slurm_testing
-GIT_REPO_TEST=${home_dir}/slurm_testing_callback
-DATA=${home_dir}/DATA/reg_5mm_pack
-OUT=${home_dir}/${IMAGE_NAME}
+GIT_REPO=${HOME_DIR}/slurm_testing
+GIT_REPO_TEST=${HOME_DIR}/slurm_testing_callback
+DATA=${HOME_DIR}/DATA/reg_5mm_pack
+OUT=${HOME_DIR}/${IMAGE_NAME}
 IMAGE=${IMAGE_NAME}.sif
 PIPELINE_CONFIGS=${DATA}/configs
 PRECONFIGS="default"
 DATA_SOURCE="Site-CBIC Site-SI HNU_1"
 
 cd $GIT_REPO_TEST
-gh repo set-default $OWNER/slurm_testing
-if [ $? -ne 0 ]
-then
-  gh workflow run "Test run failed" -F ref=$SHA -F repo=$REPO -F owner=$OWNER -F log="$(cat ${home_dir}/logs/${SHA}/error.log)"
-else
-  gh workflow run "Correlate Regression Test" -F ref=$SHA -F pipeline1="one" -F pipeline2="two" -F repo=$REPO -F owner=$OWNER -F log="$(cat ${home_dir}/logs/${SHA}/out.log)"
-fi
 
-# for pipeline in ${PRECONFIGS}; do
-#     for data in ${DATA_SOURCE}; do
-#         if [ ${data} == 'HNU_1' ]; then
-#             subject="sub-0025428"
-#             datapath=${DATA}/data/HNU_1
-#         elif [ ${data} == 'Site-CBIC' ]; then
-#             subject="sub-NDARAB348EWR"
-#             datapath=${DATA}/data/Site-CBIC
-#         elif [ ${data} == 'Site-SI' ]; then
-#             subject="sub-NDARAD481FXF"
-#             datapath=${DATA}/data/Site-SI
-#         fi
+for pipeline in ${PRECONFIGS}; do
+    for data in ${DATA_SOURCE}; do
+        if [ ${data} == 'HNU_1' ]; then
+            subject="sub-0025428"
+            datapath=${DATA}/data/HNU_1
+        elif [ ${data} == 'Site-CBIC' ]; then
+            subject="sub-NDARAB348EWR"
+            datapath=${DATA}/data/Site-CBIC
+        elif [ ${data} == 'Site-SI' ]; then
+            subject="sub-NDARAD481FXF"
+            datapath=${DATA}/data/Site-SI
+        fi
 
-#         OUTPUT=${OUT}/${pipeline}/${data}
-#         [ ! -d  ${OUTPUT} ] && mkdir -p ${OUTPUT}
+        OUTPUT=${OUT}/${pipeline}/${data}
+        [ ! -d  ${OUTPUT} ] && mkdir -p ${OUTPUT}
 
-#         cat << TMP > reglite_${IMAGE_NAME}_${pipeline}_${data}.sh
-# #!/usr/bin/bash
-# #SBATCH -N 1
-# #SBATCH -p RM-shared
+        cat << TMP > reglite_${IMAGE_NAME}_${pipeline}_${data}.sh
+#!/usr/bin/bash
+#SBATCH -N 1
+#SBATCH -p RM-shared
 # #SBATCH -t 10:00:00
 # #SBATCH --ntasks-per-node=11
-# #SBATCH -o slurm-${pipeline}-${data}.out
+#SBATCH -o slurm-${pipeline}-${data}.out
+#SBATCH --wrap "OWNER=$OWNER REPO=$REPO SHA=$SHA HOME_DIR=$HOME_DIR ./$GIT_REPO/.github/scripts/status.SLRUM"
 
-# SINGULARITY_CACHEDIR=${home_dir}/.singularity/cache \
-# SINGULARITY_LOCALCACHEDIR=${home_dir}/.singularity/tmp \
+# SINGULARITY_CACHEDIR=${HOME_DIR}/.singularity/cache \
+# SINGULARITY_LOCALCACHEDIR=${HOME_DIR}/.singularity/tmp \
 # singularity build ${IMAGE} docker://${image}
 
 # singularity run \
 #     --cleanenv \
-#     -B ${home_dir} \
+#     -B ${HOME_DIR} \
 #     -B ${datapath}:/data \
 #     -B ${OUTPUT}:/outputs \
 #     -B ${PIPELINE_CONFIGS}:/pipeline_configs \
@@ -76,5 +70,5 @@ fi
 #     done
 # done
 
-# rm reglite_${IMAGE_NAME}_*.sh
+rm reglite_${IMAGE_NAME}_*.sh
 # rm ${IMAGE}
