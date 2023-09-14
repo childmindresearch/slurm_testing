8f98c7abeeb8f1cd31e77b77685e3750b500e724
Jon Clucas <jon.clucas@childmind.org>
2023-09-14 13:26:12 -0400

    :mute: Reduce logging

--- .github/scripts/wait_for_git_lock.sh
+++ .github/scripts/wait_for_git_lock.sh
@@ -4,8 +4,7 @@
 # Function to check if .git/index.lock exists and wait until it's gone
 function wait_for_git_lock {
     while [ -f "$GIT_REPO_DIR/.git/index.lock" ]; do
-        echo "A Git lock file (.git/index.lock) is present. Waiting..."
+        >&2 echo "$(date)    A Git lock file (.git/index.lock) is present. Waiting…"
         sleep 5
     done
-    echo "No Git lock file found. Proceeding."
 }
