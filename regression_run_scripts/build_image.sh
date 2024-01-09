53eba4b853bb8e47e9edaa7b3b374dcdb10465c5
Jon Clucas <jon.clucas@childmind.org>
2024-01-09 18:58:18 -0500

    :necktie: Clear Singularity cache before building new image

--- regression_run_scripts/build_image.sh
+++ regression_run_scripts/build_image.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-set -x
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --working_dir) working_dir="$2"; shift ;;
@@ -13,9 +11,9 @@ done
 image_name=${image#*:}
 for _DIR in cache tmp
 do
-  mkdir -p ${working_dir}/.singularity/${_DIR}
+  mkdir -p "${working_dir}/.singularity/${_DIR}"
 done
-cat << TMP > job.sh
+cat << TMP > "build_${image_name}.sh"
 #!/usr/bin/bash
 #SBATCH -N 1
 #SBATCH -p RM-shared
@@ -24,16 +22,15 @@ cat << TMP > job.sh
 #SBATCH -o ${working_dir}/logs/${SHA}/launch/out.log
 #SBATCH --error ${working_dir}/logs/${SHA}/launch/error.log
 
-set -x
-
-SINGULARITY_CACHEDIR=${working_dir}/.singularity/cache \
-SINGULARITY_LOCALCACHEDIR=${working_dir}/.singularity/tmp \
+export SINGULARITY_CACHEDIR=${working_dir}/.singularity/cache \
+       SINGULARITY_LOCALCACHEDIR=${working_dir}/.singularity/tmp
+yes | singularity cache clean
 yes | singularity build ${working_dir}/${image_name}.sif docker://${image}
 
 TMP
 
-chmod +x job.sh
-sbatch --wait job.sh
+chmod +x "build_${image_name}.sh"
+sbatch --wait "build_${image_name}.sh"
 EXIT_CODE=$?
-rm job.sh
+rm "build_${image_name}.sh"
 exit $EXIT_CODE
