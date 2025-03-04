6817553e773999cc1f22aaeba6b04b904ba44f6c
Jon Clucas <jon.clucas@childmind.org>
2025-03-04 10:12:37 -0500

    :truck: Move correlations from `SHA/scope` to `scope/SHA`

--- src/cpac_slurm_testing/status/status.py
+++ src/cpac_slurm_testing/status/status.py
@@ -550,7 +550,7 @@ class TotalStatus:
 
     def out(self) -> Path:
         """Return the path to the output directory."""
-        return ExistingPath(self.home_dir / self.image("name") / self.scope)
+        return ExistingPath(self.home_dir / self.scope / self.image("name"))
 
     def check(self: "TotalStatus", args: Namespace) -> None:
         """Check a run's status."""
