7330f08201e60cdd03c2c730703bcfa7ca6eaa91
Jon Cluce <jon.clucas@childmind.org>
2025-03-05 12:10:40 -0500

    :necktie: Make sure output path exists before trying to bind

--- src/cpac_slurm_testing/status/status.py
+++ src/cpac_slurm_testing/status/status.py
@@ -313,7 +313,7 @@ class RunStatus:
             log_dir=self.log_dir,
             image=self.total.image("path"),
             image_name=self.total.image("name"),
-            output=self.out() / self.preconfig / self.data_source,
+            output=ExistingPath(self.out() / self.preconfig / self.data_source),
             pdsd=self.pdsd,
             pipeline=self.preconfig,
             pipeline_configs=str(
